1e691766b8825fa23bf7cad8ac7a06a3b05a4b38
kevin <kevinlee35.xx@gmail.com>
2021-12-02 21:05:45 +0800

    support directory scan to product lists

--- new_ocean.py
+++ new_ocean.py
@@ -14,11 +14,11 @@ import xlwt
 import xlrd
 
 # 此部分需根据产品线手动添加
-product_list = ["00.文档模板", "01.深海大数据平台", "02.基础平台", "03.门户", "04.资源管理平台", "05.数据集成平台", "06-1.作业管理平台", "06.数据开发平台", "07.数据治理平台", "08.实时数据集成", "09.数据可视化平台-BI", "09.数据可视化平台-A+", "10.标签画像平台", "11.实时计算平台", "12.变量中心服务", "13.数据服务平台", "14.指标管理平台", "15.外采管理平台", "16.机器学习平台", "17.图分析平台"]
-doc_list = ["01产品文档", "02技术文档", "03测试文档", "04部署运维手册"]
-doc_detail_list = ["01.需求说明书", "02.产品宣传", "03.产品白皮书", "04.产品原型", "05.用户操作手册", "06.产品功能清单", "07.里程碑清单", "01.代码库清单", "02.总体设计", "03.概要设计", "04.详细设计", "05.数据库设计", "06.产品依赖说明", "01.测试计划", "02.测试用例", "03.测试报告", "01.安装操作说明", "02.测试&生产资源台账", "03.安装验证清单", "02.部署初始化脚本", "03.产品升级说明", "04.系统运维手册", "05.应急处置手册"]
-segment_arr = [7, 13, 16, 23] # doc名称分界index
-version_list = ["V", "V1.0", "V2.0", "V2.1"]
+product_list = []
+doc_list = []
+doc_detail_list = []
+segment_arr = [] # doc名称分界index
+version_list = ["V"] # "V"用于文档模板一栏
 ignore_files = [".git", ".gitkeep", ".gitignore", "README.md"]
 ignore_docs = ["XX.其他文档"]
 example_prod = "00.文档模板"
@@ -27,10 +27,60 @@ example_prod = "00.文档模板"
 wb = xlwt.Workbook(encoding = 'utf-8')
 sh = wb.add_sheet('ocean_doc', cell_overwrite_ok=True)
 
-# TODO(Kevin): 通过os.walk或os.listdir获取每一层的内容并进行筛选
+# TODO(Kevin): 通过os.listdir获取每一层的内容并进行筛选
 #-------------------------------------------------------
+path = "./ocean_doc"
+# 1.登记文档名称 | 2.文档细节名称 | 3.分隔文档类型
+# (通过00.文档模板获取)
+example_path = path + "/" + example_prod
+example_dirs = os.listdir(example_path)
+count = 0
+for dir1 in example_dirs:
+    if dir1 in ignore_files or dir1 in ignore_docs:
+        continue
+    doc_list.append(dir1)
+    curr_example_path = example_path + "/" + dir1
+    curr_example_dirs = os.listdir(curr_example_path)
+    inner_count = 0
+    for dir2 in curr_example_dirs:
+        check_path = curr_example_path + "/" + dir2
+        check_dir = os.listdir(check_path)
+        if (os.path.isdir(check_path + "/" + check_dir[0])):
+            inner_count = inner_count + len(check_dir) 
+            for dir3 in check_dir:
+                doc_detail_list.append(dir3)
+        else:
+            count = count + 1
+            doc_detail_list.append(dir2)
+    count = count + inner_count
+    segment_arr.append(count)
 
+# 1.标记产品列表 | 2.版本列表 & 3.检查是否有产品在第二层目录分类
+dirs = os.listdir(path)
+for dir1 in dirs:
+    if dir1 in ignore_files:
+        continue
+    product_list.append(dir1) # 标记产品
 
+    next_path = path + "/" + dir1
+    next_dirs = os.listdir(next_path)
+    
+    prod_ver_count = 0
+    curr_index = 0
+    for dir2 in next_dirs:
+        inner_next_dirs = os.listdir(next_path + "/" + dir2)
+        if dir2[0] == 'V' and dir2 not in version_list:
+            version_list.append(dir2)
+        elif inner_next_dirs[0][0] == 'V': # 如果version在下一层
+            if (prod_ver_count == 0):
+                curr_index = product_list.index(dir1)
+                product_list.remove(dir1)
+            product_list.insert((curr_index+prod_ver_count), dir1+dir2.split('.')[1])
+            prod_ver_count = prod_ver_count + 1
+            for inner_next_dir in inner_next_dirs:
+                if inner_next_dir not in version_list:
+                    version_list.append(inner_next_dir)
+version_list = sorted(version_list)
 
 
 
--- scan_test.py
+++ scan_test.py
@@ -7,8 +7,7 @@ product_list = []
 doc_list = []
 doc_detail_list = []
 segment_arr = [] # doc名称分界index
-version_list = ["V"]
-version_list = sorted(version_list)
+version_list = ["V"] # "V"用于文档模板一栏
 ignore_files = [".git", ".gitkeep", ".gitignore", "README.md"]
 ignore_docs = ["XX.其他文档"]
 example_prod = "00.文档模板"
@@ -20,16 +19,10 @@ sh = wb.add_sheet('ocean_doc', cell_overwrite_ok=True)
 # TODO(Kevin): 通过os.listdir获取每一层的内容并进行筛选
 #-------------------------------------------------------
 path = "./ocean_doc"
-dirs = os.listdir(path)
-# 登记第一层文件夹
-for dir1 in dirs:
-    if dir1 in ignore_files:
-        continue
-    product_list.append(dir1)
-
+# 1.登记文档名称 | 2.文档细节名称 | 3.分隔文档类型
+# (通过00.文档模板获取)
 example_path = path + "/" + example_prod
 example_dirs = os.listdir(example_path)
-# 登记第二层文件夹(通过00.文档模板获取)
 count = 0
 for dir1 in example_dirs:
     if dir1 in ignore_files or dir1 in ignore_docs:
@@ -51,13 +44,38 @@ for dir1 in example_dirs:
     count = count + inner_count
     segment_arr.append(count)
 
-#标记版本号
+# 1.标记产品列表 | 2.版本列表 & 3.检查是否有产品在第二层目录分类
+dirs = os.listdir(path)
 for dir1 in dirs:
     if dir1 in ignore_files:
         continue
-    product_list.append(dir1)
+    product_list.append(dir1) # 标记产品
+
+    next_path = path + "/" + dir1
+    next_dirs = os.listdir(next_path)
+    
+    prod_ver_count = 0
+    curr_index = 0
+    for dir2 in next_dirs:
+        inner_next_dirs = os.listdir(next_path + "/" + dir2)
+        if dir2[0] == 'V' and dir2 not in version_list:
+            version_list.append(dir2)
+        elif inner_next_dirs[0][0] == 'V': # 如果version在下一层
+            print(prod_ver_count)
+            if (prod_ver_count == 0):
+                curr_index = product_list.index(dir1)
+                product_list.remove(dir1)
+            product_list.insert((curr_index+prod_ver_count), dir1+dir2.split('.')[1])
+            print(product_list.index(dir1+dir2.split('.')[1]))
+            prod_ver_count = prod_ver_count + 1
+            for inner_next_dir in inner_next_dirs:
+                if inner_next_dir not in version_list:
+                    version_list.append(inner_next_dir)
+version_list = sorted(version_list)
+
 
 # print(product_list)
 # print(doc_list)
 # print(doc_detail_list)
-print(segment_arr)
+# print(segment_arr)
+# print(version_list)
