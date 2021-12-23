328821f9beb763f10f8f1ab206473ee7e7f3f577
kevin <kevinlee35.xx@gmail.com>
2021-12-23 19:45:05 +0800

    get example doc based on user input

--- new_ocean.py
+++ new_ocean.py
@@ -35,8 +35,10 @@ segment_arr = []  # doc名称分界index
 version_list = ["V"]  # "V"用于文档模板一栏
 ignore_files = [".git", ".gitkeep", ".gitignore", "README.md"]
 ignore_docs = ["XX.其他文档"]
-example_prod = "00.文档模板"
+example_prod = input("请输入文档模板名称（请仔细核对）：")
+example_prod = "00.文档模板" # 暂时作为模板文档名
 each_version_dict = {example_prod: ["V"]}
+required_doc = input("请输入必须标注的文件（请以文件夹名为准，并以空格隔开）：").split(" ")
 required_doc = ["03.产品白皮书", "05.用户操作手册", "01.代码库清单", "03.概要设计", "05.数据库设计", "01.安装操作说明", "02.部署初始化脚本"]
 # ----------------------------------------------------
 
