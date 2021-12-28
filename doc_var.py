aa89cd0e7fc64a18b998073d96212a0f8316823d
kevin <kevinlee35.xx@gmail.com>
2021-12-28 18:25:25 +0800

    get overall stats based on groups

---
+++ doc_var.py
@@ -0,0 +1,10 @@
+path = "./ocean_doc"
+
+group_division = ["产品", "开发", "测试", "运维"]
+platform_name = ["深海", "ocean"]
+ignore_files = [".git", ".gitkeep", ".gitignore", "README.md"]
+ignore_docs = ["XX.其他文档"]
+# example_prod = input("请输入文档模板名称（请仔细核对）：")
+example_prod = "00.文档模板" # 暂时作为模板文档名
+# required_doc = input("请输入必须标注的文件（请以文件夹名为准，并以空格隔开）：").split(" ")
+required_doc = ["03.产品白皮书", "05.用户操作手册", "01.代码库清单", "03.概要设计", "05.数据库设计", "01.安装操作说明", "02.部署初始化脚本"]
